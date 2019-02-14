43e687a4026adc5a6bf9d351a32a7a40def297d6
mxh <muxiaohan1995@gmail.com>
2019-02-13 23:45:10 -0800

    Updates

---
+++ precache-manifest.6d5fab87475a742a9e2124ea0d7a7e63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc76fca5f2403bd07f79",
+    "url": "/test-deploy/static/js/runtime~main.cc76fca5.js"
+  },
+  {
+    "revision": "96f81ae627f27310b410",
+    "url": "/test-deploy/static/js/main.96f81ae6.chunk.js"
+  },
+  {
+    "revision": "e2cc601ea92518eb51f2",
+    "url": "/test-deploy/static/js/2.e2cc601e.chunk.js"
+  },
+  {
+    "revision": "96f81ae627f27310b410",
+    "url": "/test-deploy/static/css/main.0248d003.chunk.css"
+  },
+  {
+    "revision": "c21de05646970f0b77e7a8f78383d173",
+    "url": "/test-deploy/index.html"
+  }
+];
